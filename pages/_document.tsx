97bf44d4f2c4163baee3372cf3299de36d7a00c0
Kamil Sarga <kamil.sarga@live.com>
2022-08-29 20:26:41 +0200

    Added font and changed place of headers

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="pl">
+			<Head>
+				<title>Salon Urody Estetica</title>
+				<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&family=Open+Sans&family=Satisfy&display=swap"
+				      rel="stylesheet"/>
+			</Head>
+			<body>
+			<Main/>
+			<NextScript/>
+			</body>
+		</Html>
+	);
+}
