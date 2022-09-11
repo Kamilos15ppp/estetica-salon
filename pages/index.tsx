241607793bd7cfb299bacd28e146368576f06c1f
Kamil Sarga <kamil.sarga@live.com>
2022-09-11 09:40:50 +0200

    Added rwd for Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,21 @@ const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
 			<h6 className={styles.header}>{M.homepage.header}</h6>
-			<div className={styles.textContent}>
-				<p className={styles.text1}>{M.homepage.textContent1}</p>
-				<p className={styles.text2}>{M.homepage.textContent2}</p>
-				<span className={styles.signature}>{M.homepage.signature}</span>
+			<div className={styles.innerContainer}>
+				<div className={styles.textContent}>
+					<p className={styles.text1}>{M.homepage.textContent1}</p>
+					<p className={styles.text2}>{M.homepage.textContent2}</p>
+					<span className={styles.signature}>{M.homepage.signature}</span>
+				</div>
+				<div className={styles.image}>
+					<Image
+						src={woman}
+						alt={M.homepage.signature}
+						width={320}
+						height={500}
+					/>
+				</div>
 			</div>
-			<Image
-				className={styles.image}
-				src={woman}
-				alt={M.homepage.signature}
-				width={320}
-				height={500}
-			/>
 		</div>
 	);
 };
