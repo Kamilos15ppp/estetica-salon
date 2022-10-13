0949a9d25bfed7a926766f6d36dadc0c52f42392
Kamil Sarga <kamil.sarga@live.com>
2022-10-13 13:24:12 +0200

    Fixed DOM structure and styles for Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,20 +9,18 @@ import styles from '../styles/homepage.module.scss';
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.innerContainer}>
-				<div className={styles.textContent}>
-					<h6 className={styles.header}>{M.homepage.header}</h6>
-					<p className={styles.text1}>{M.homepage.textContent1}</p>
-					<p className={styles.text2}>{M.homepage.textContent2}</p>
-					<span className={styles.signature}>{M.homepage.signature}</span>
-				</div>
-				<div className={styles.image}>
-					<Image
-						src={woman}
-						alt={M.homepage.signature}
-						layout="fill"
-					/>
-				</div>
+			<div className={styles.textContent}>
+				<h6 className={styles.header}>{M.homepage.header}</h6>
+				<p className={styles.text1}>{M.homepage.textContent1}</p>
+				<p className={styles.text2}>{M.homepage.textContent2}</p>
+				<span className={styles.signature}>{M.homepage.signature}</span>
+			</div>
+			<div className={styles.image}>
+				<Image
+					src={woman}
+					alt={M.homepage.signature}
+					layout="fill"
+				/>
 			</div>
 		</div>
 	);
